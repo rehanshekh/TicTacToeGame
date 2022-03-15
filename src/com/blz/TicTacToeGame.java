c87087013c7a378975ba54b75a0d141937c8f033
Rehan Shekh <rehanrshekh@gmail.com>
2022-03-15 20:33:10 +0530

    UC_2 Done, added method for input

--- src/com/blz/TicTacToeGame.java
+++ src/com/blz/TicTacToeGame.java
@@ -1,4 +1,24 @@
 package com.blz;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
+
+    public static void main(String[] args) {
+        char[] board = new char[10];
+        for (int i = 1; i < 10; i++) {
+            board[i] = ' ';
+        }
+        char inputUser = input();
+        if (inputUser == 'X') {
+            char inputC = 'O';
+        }
+    }
+
+    public static char input() {
+        System.out.println("Please enter your input in X / O");
+        Scanner in = new Scanner(System.in);
+        char inputU = in.next().charAt(0);
+        return inputU;
+    }
 }
