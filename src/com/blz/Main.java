e7d62f799405ea1f8ae0875ce979e863f9991e11
Rehan Shekh <rehanrshekh@gmail.com>
2022-03-15 19:54:37 +0530

    UC_1 Done

---
+++ src/com/blz/Main.java
@@ -0,0 +1,11 @@
+package com.blz;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char[] board = new char[10];
+        for (int i = 1; i <= 10; i++) {
+            board[i] = ' ';
+        }
+    }
+}
---
+++ src/com/blz/TicTacToeGame.java
@@ -0,0 +1,4 @@
+package com.blz;
+
+public class TicTacToeGame {
+}
